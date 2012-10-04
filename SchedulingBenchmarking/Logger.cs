bec64b1fb802c87aae15125369d023148c431185
ungeroed <morten@fam-f.dk>
2012-10-04 16:39:42 +0200

    now with the data

---
+++ SchedulingBenchmarking/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchedulingBenchmarking
+{
+    public class Logger
+    {
+        /// <summary>
+        /// Method invoked by any state change in BenchMarkSystem. Publishes a running commentary 
+        /// when any job is submitted, cancelled, run, failed or terminated
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void OnStateChanged(object sender, StateChangedEventArgs e)
+        {
+            Console.WriteLine("Job state {0}", e.State);
+        }
+    }
+}
---
+++ SchedulingBenchmarking/StateChangedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchedulingBenchmarking
+{
+    public class StateChangedEventArgs : EventArgs
+    {
+        public State State
+        {
+            get;
+            set;
+        }
+    }	
+}
